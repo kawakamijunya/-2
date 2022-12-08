20dfd547073cc517c8a0a6935ac9c8c47768d72c
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2022-12-08 07:01:04 +0000

    [Finish]4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,34 @@
 class PostImagesController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @post_image=PostImage.new
   end
+
+  #投稿データの保存
+  def create
+    @post_image=PostImage.new(post_image_params)
+    @post_image.user_id=current_user.id
+    @post_image.save
+    redirect_to post_images_path #indexページへリダイレクト
+  end
+
 
   def index
   end
 
   def show
   end
+
+  def destroy
+  end
+
+  #投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name,:image,:caption)
+  end
+
 end
+
+
--- db/migrate/20221208023957_create_post_images.rb
+++ db/migrate/20221208023957_create_post_images.rb
@@ -1,6 +1,6 @@
-class CreatePostimages < ActiveRecord::Migration[6.1]
+class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
-    create_table :postimages do |t|
+    create_table :post_images do |t|
 
       t.string :shop_name
       t.text :caption
