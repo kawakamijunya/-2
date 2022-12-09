d0634317dab371d5249f45407a8293ee0b0a8315
EC2 Default User <ec2-user@ip-172-31-45-76.ap-northeast-1.compute.internal>
2022-12-09 08:42:56 +0000

    [Finish]4-21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   belongs_to :user #PostImageモデルに関連付けられるUserモデルは、1つ
   has_many :post_comments,dependent: :destroy #PostCommentモデルとの1:Nの関係
   has_many :favorites,dependent: :destroy
+  
+  validates :shop_name,presence: true #shop_nameが存在しているかを確認するバリデーション
+  validates :image,presence: true #imageが存在しているかを確認するバリデーション
 
 
   def get_image
@@ -13,11 +16,10 @@ class PostImage < ApplicationRecord
     end
     image
   end
-  
-  
+
+
   def favorited_by?(user) #このメソッドで、引数で渡されたユーザidがFavoritesテーブル内に存在（exists?）するかどうかを調べます
     favorites.exists?(user_id: user.id) #存在していればtrue、存在していなければfalseを返すようにしています。
-
   end
 
 end
